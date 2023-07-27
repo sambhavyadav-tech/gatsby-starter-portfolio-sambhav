735496ee82076335e7be5a93b4f021f4677b0f4c
sambhav yadav <sambhavyadav25@gmail.com>
2023-07-27 17:24:09 +0000

    # update config.ts

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -8,14 +8,14 @@ const config: GatsbyConfig = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.mjs
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://cara.lekoarts.de`,
+    siteTitle: `Sambhav`,
+    siteTitleAlt: `Sambhav Yadav - Gatsby Starter Portfolio`,
+    siteHeadline: `Sambhav Yadav - Portfolio`,
+    siteUrl: `https://peaceful-dieffenbachia-784038.netlify.app/`,
     siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
     siteImage: `/banner.jpg`,
     siteLanguage: `en`,
-    author: `@lekoarts_de`,
+    author: `@sambahv.yadav`,
   },
   trailingSlash: `never`,
   plugins: [
